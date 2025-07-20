d41d881028770f42ca90f5bd479b2caf14a3e8ce
aminul-islam-sajid <mdaminulislamsajid@gmail.com>
2025-07-20 10:18:20 +0600

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import Nevbar from "./components/navbar.component";
+import UserAUthForm from "./pages/userAuthForm.page";
 
 const App = () => {
-    return (
-        <Nevbar/>
-    )
-}
+  return (
+    <Routes>
+      <Route path="/" element={<Nevbar />} >
+      <Route path="signin" element={ <UserAUthForm type="sign-in"/> } />
+      <Route path="signup" element={ <UserAUthForm type="sign-up"/> } />
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;
---
+++ src/pages/userAuthForm.page.jsx
@@ -0,0 +1,12 @@
+const UserAUthForm = ({ type }) => {
+  return (
+    <section className="h-cover flex items-center justify-center">
+      <form className="w-[80%] max-w-[400px]">
+        <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
+          {type === "signup" ? "Welcome Back" : "Join us Today"}
+        </h1>
+      </form>
+    </section>
+  );
+};
+ export default UserAUthForm;
